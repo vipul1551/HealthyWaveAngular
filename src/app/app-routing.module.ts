fd0f790cb318f49e7ec4c98752794828e5d0a488
Vipul Prajapati <vipul1551>
2022-09-04 14:49:51 +0530

    angular template for healthywave

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AdminLayoutComponent } from './admin/admin-layout/admin-layout.component';
+import { HomeComponent } from './user/home/home.component';
+import { UserLayoutComponent } from './user/user-layout/user-layout.component';
+const routes: Routes = [
+  {component : UserLayoutComponent,path : "user",children: [
+    {component:HomeComponent,path : "home"}
+  ]
+  },
+  {component : AdminLayoutComponent,path : "admin",children: [
+    // {component : AdminComponent,path : "dashboard"}
+  ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserLayoutComponent } from './user/user-layout/user-layout.component';
+import { AdminLayoutComponent } from './admin/admin-layout/admin-layout.component';
+import { HomeComponent } from './user/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserLayoutComponent,
+    AdminLayoutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
