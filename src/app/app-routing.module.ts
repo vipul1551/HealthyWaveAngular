6a580f4d56c4281f0392e4ca0fa3ef5ad1c264ea
mittal1122 <mittalkumavat6676@gmail.com>
2022-09-04 20:57:07 +0530

    topbar home footer login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminLayoutComponent } from './admin/admin-layout/admin-layout.component';
+import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './user/home/home.component';
 import { UserLayoutComponent } from './user/user-layout/user-layout.component';
 const routes: Routes = [
-  {component : UserLayoutComponent,path : "user",children: [
-    {component:HomeComponent,path : "home"}
-  ]
+  {
+    component: UserLayoutComponent, path: "user", children: [
+      { component: HomeComponent, path: "home" },
+      { component: LoginComponent, path: "login" }
+    ]
   },
-  {component : AdminLayoutComponent,path : "admin",children: [
-    // {component : AdminComponent,path : "dashboard"}
-  ]
+  {
+    component: AdminLayoutComponent, path: "admin", children: [
+    ]
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { UserLayoutComponent } from './user/user-layout/user-layout.component';
 import { AdminLayoutComponent } from './admin/admin-layout/admin-layout.component';
 import { HomeComponent } from './user/home/home.component';
+import { TopBarComponent } from './user/top-bar/top-bar.component';
+import { HeaderComponent } from './user/header/header.component';
+import { FooterComponent } from './user/footer/footer.component';
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     UserLayoutComponent,
     AdminLayoutComponent,
-    HomeComponent
+    HomeComponent,
+    TopBarComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    
   ],
   imports: [
     BrowserModule,
