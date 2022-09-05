924aaad394bb15dea6322b73adfb13ef0c700bb1
Vipul Prajapati <vipul1551>
2022-09-05 19:30:41 +0530

    signup form done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminLayoutComponent } from './admin/admin-layout/admin-layout.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 import { HomeComponent } from './user/home/home.component';
 import { UserLayoutComponent } from './user/user-layout/user-layout.component';
 const routes: Routes = [
   {
     component: UserLayoutComponent, path: "", children: [
       { component: HomeComponent, path: "" },
-      { component: LoginComponent, path: "login" },]
+      { component: LoginComponent, path: "login" },
+      { component: SignupComponent, path: "signup"}
+    ]
   },
   {
     component: AdminLayoutComponent, path: "admin", children: [
